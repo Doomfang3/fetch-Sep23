5deac2861b4845e7ddb240775ccf4d787b40d403
Doomfang3 <saubinmathieu@gmail.com>
2023-10-20 12:59:48 +0200

    The rest

--- backend/routes/characters.routes.js
+++ backend/routes/characters.routes.js
@@ -27,6 +27,58 @@ router.get('/:id', async (req, res) => {
   }
 })
 
+// POST to create a new character
+router.post('/', async (req, res) => {
+  try {
+    const responseFromAPI = await fetch(`https://ih-crud-api.herokuapp.com/characters`, {
+      method: 'POST',
+      body: JSON.stringify(req.body),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    if (responseFromAPI.ok) {
+      const newCharacter = await responseFromAPI.json()
+      res.status(201).json(newCharacter)
+    }
+  } catch (error) {
+    console.log(error)
+  }
+})
+// PUT to update an existing character
+router.put('/:id', async (req, res) => {
+  console.log(req.body)
+  try {
+    const responseFromAPI = await fetch(
+      `https://ih-crud-api.herokuapp.com/characters/${req.params.id}`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(req.body),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    )
+    if (responseFromAPI.ok) {
+      const characterFromAPI = await responseFromAPI.json()
+      res.json({ character: characterFromAPI })
+    }
+  } catch (error) {
+    console.error(error)
+  }
+})
+// DELETE to delete one character
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params
+  try {
+    await fetch(`https://ih-crud-api.herokuapp.com/characters/${id}`, {
+      method: 'DELETE',
+    })
+    res.status(204).send()
+  } catch (error) {
+    res.status(500).json({ error })
+  }
+})
 module.exports = router
 
 // https://ih-crud-api.herokuapp.com/characters
